840a6de39013a989768a7ca104e874783613173b
wzsvip <161473552+wzsvip@users.noreply.github.com>
2024-10-17 09:29:02 +0800

    Update fofa.sh

--- speedtest/fofa.sh
+++ speedtest/fofa.sh
@@ -28,7 +28,7 @@ if [ $# -eq 0 ]; then
   echo "13. 湖南电信（Hunan_282）"
   echo "14. 甘肃电信（Gansu_105）"
   echo "15. 河北联通（Hebei_313）"
-  echo "16. 重庆联通（Chongqing_156）" 
+  echo "16. 重庆联通（Chongqing_liantong_156）" 
   echo "0. 全部"
   read -t 10 -p "输入选择或在10秒内无输入将默认选择全部: " city_choice
 
@@ -150,7 +150,7 @@ case $city_choice in
         url_fofa="https://fofa.info/result?qbase64="$url_fofa
         ;;
      16)
-        city="Chongqing_156"
+        city="Chongqing_liantong_156"
         stream="udp/225.0.4.197:7980"
         channel_key="重庆联通"
         url_fofa=$(echo ""udpxy" && country="CN" && region="Chongqing"  && protocol="http"" | base64)
@@ -216,7 +216,7 @@ while IFS= read -r line; do
     url="http://$ip/$stream"
     echo "$url"
     curl "$url" --connect-timeout 3 --max-time 10 -o /dev/null >zubo.tmp 2>&1
-    a=$(head -n 3 zubo.tmp | awk '{print $NF}' | tail -n 1)
+    a=$(head -n 5 zubo.tmp | awk '{print $NF}' | tail -n 1)
 
     echo "第 $i/$lines 个：$ip $a"
     echo "$ip $a" >> "speedtest_${city}_$time.log"
@@ -228,17 +228,21 @@ cat "result/result_fofa_${city}.txt"
 ip1=$(awk 'NR==1{print $2}' result/result_fofa_${city}.txt)
 ip2=$(awk 'NR==2{print $2}' result/result_fofa_${city}.txt)
 ip3=$(awk 'NR==3{print $2}' result/result_fofa_${city}.txt)
+ip4=$(awk 'NR==4{print $2}' result/result_fofa_${city}.txt)
+ip5=$(awk 'NR==5{print $2}' result/result_fofa_${city}.txt)
 rm -f "speedtest_${city}_$time.log"
 
-# 用 3 个最快 ip 生成对应城市的 txt 文件
+# 用 5 个最快 ip 生成对应城市的 txt 文件
 program="template/template_${city}.txt"
 
 sed "s/ipipip/$ip1/g" "$program" > tmp1.txt
 sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
 sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
-cat tmp1.txt tmp2.txt tmp3.txt > "txt/fofa_${city}.txt"
+sed "s/ipipip/$ip4/g" "$program" > tmp4.txt
+sed "s/ipipip/$ip5/g" "$program" > tmp5.txt
+cat tmp1.txt tmp2.txt tmp3.txt tmp4.txt tmp5.txt > "txt/fofa_${city}.txt"
 
-rm -rf tmp1.txt tmp2.txt tmp3.txt
+rm -rf tmp1.txt tmp2.txt tmp3.txt tmp4.txt tmp5.txt
 
 
 #--------------------合并所有城市的txt文件为:   zubo_fofa.txt-----------------------------------------
@@ -274,7 +278,7 @@ cat txt/Gansu_105.txt >>zubo_fofa.txt
 echo "河北联通,#genre#" >>zubo_fofa.txt
 cat txt/Hebei_313.txt >>zubo_fofa.txt
 echo "重庆联通,#genre#" >>zubo_fofa.txt
-cat txt/Chongqing_156.txt >>zubo_fofa.txt
+cat txt/Chongqing_liantong_156.txt >>zubo_fofa.txt
 
 
 for a in result/*.txt; do echo "";echo "========================= $(basename "$a") ==================================="; cat $a; done
